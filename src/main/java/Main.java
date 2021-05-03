c793c891b72dcece4bb979508b01a56ed5631e1f
VanRar <vitalikf7@gmail.com>
2021-05-03 21:59:45 +0300

    убрал свои комментарии

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,7 @@ public class Main {
         List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
         List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
         Collection<Person> persons = new ArrayList<>();
-        //пока сократим до 100, что бы можно было глянуть как работает
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 10_000_000; i++) {
             persons.add(new Person(
                     names.get(new Random().nextInt(names.size())),
                     families.get(new Random().nextInt(families.size())),
@@ -16,28 +15,21 @@ public class Main {
                     Education.values()[new Random().nextInt(Education.values().length)])
             );
         }
-        //глянем что там вышло
-        System.out.println(persons);
 
         int countOfMinors = (int) persons.stream()
                 .filter(p -> p.getAge() < 18)
                 .count();
-        System.out.println(countOfMinors);
 
         List<String> listOfConscript = persons.stream()
                 .filter(p -> p.getSex() == Sex.MAN)
                 .filter(p -> p.getAge() > 18 & p.getAge() < 27)
                 .map(Person::getFamily)
                 .collect(Collectors.toList());
-        System.out.println(listOfConscript);
 
         List<Person> listWorkable = persons.stream()
                 .filter(p -> p.getEducation() == Education.HIGHER)
-                .filter(p -> (p.getSex()==Sex.WOMEN & p.getAge() > 18 & p.getAge() < 60)|(p.getSex() ==Sex.MAN & p.getAge() > 18 & p.getAge() < 65))
+                .filter(p -> (p.getSex() == Sex.WOMEN & p.getAge() > 18 & p.getAge() < 60) | (p.getSex() == Sex.MAN & p.getAge() > 18 & p.getAge() < 65))
                 .sorted(Comparator.comparing(Person::getFamily))
                 .collect(Collectors.toList());
-
-        System.out.println();
-        System.out.println(listWorkable);
     }
 }
